92f499ae0861a3664a6f9b6eb0f3dd0b493ac68d
Maroua Ammar <maroi.ammar22@gmail.com>
2020-07-01 10:08:03 +0200

    Updates

---
+++ precache-manifest.f98468c85f25186d13a8e3b4da4cb6c5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30872831665b331a43e373ba3d41669f",
+    "url": "/application/index.html"
+  },
+  {
+    "revision": "0e31ba163609ccff291e",
+    "url": "/application/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "08372c8ab118b51397a4",
+    "url": "/application/static/css/main.79d90f83.chunk.css"
+  },
+  {
+    "revision": "0e31ba163609ccff291e",
+    "url": "/application/static/js/2.9e5a6cf7.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/application/static/js/2.9e5a6cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08372c8ab118b51397a4",
+    "url": "/application/static/js/main.21abd2a8.chunk.js"
+  },
+  {
+    "revision": "191806e634825ee94201",
+    "url": "/application/static/js/runtime-main.8bf40372.js"
+  },
+  {
+    "revision": "bde300d68b5e077aa9fb609f96863a13",
+    "url": "/application/static/media/bg-masthead.bde300d6.jpg"
+  },
+  {
+    "revision": "859dd8b6d58a48077843eaef8fff1efe",
+    "url": "/application/static/media/bg-noise-texture.859dd8b6.png"
+  },
+  {
+    "revision": "a23e804265f4b1cba5cf60cee515ddf3",
+    "url": "/application/static/media/deadpool.a23e8042.png"
+  },
+  {
+    "revision": "ed509e33519b210c1606921e78501bf9",
+    "url": "/application/static/media/griffes-droit.ed509e33.png"
+  },
+  {
+    "revision": "e578fea6021d2318d02c9f353a3f72ad",
+    "url": "/application/static/media/griffes-gauche.e578fea6.png"
+  },
+  {
+    "revision": "248392c9463115f69738082636d79a7e",
+    "url": "/application/static/media/ironman.248392c9.png"
+  },
+  {
+    "revision": "6a1254d8df2375f4ae56aee5ae8bab09",
+    "url": "/application/static/media/spiderman.6a1254d8.png"
+  },
+  {
+    "revision": "6c4150e99b299fe7d7c78e15e31bb377",
+    "url": "/application/static/media/wolverine.6c4150e9.png"
+  },
+  {
+    "revision": "738ca667ed66ccf0fc7072dff28ab603",
+    "url": "/application/static/media/xmen.738ca667.png"
+  }
+]);
